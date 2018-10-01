79f0961da919f9de625abeb4d169308c8126dfee
achorny <moluram.a@gmail.com>
2018-10-01 11:42:04 +0300

    Solved

--- src/index.js
+++ src/index.js
@@ -1,3 +1,45 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+    return getPrimes(base)
+        .filter(prime => base >= prime && base % prime === 0)
+        .map(prime => divideBase(prime, base))
+        .map(value => countOccurrences(value, number))
+        .reduce((previousValue, currentValue) => previousValue > currentValue ?
+                currentValue : previousValue, Number.MAX_SAFE_INTEGER);
+
+};
+
+function countOccurrences(value, number) {
+    let del = value.base;
+    let count = 0;
+    let division = 2 * value.base;
+
+    while (division >= value.base) {
+        division = Math.floor(number / del);
+        del *= value.base;
+        count += division
+    }
+
+    return Math.trunc(count / value.count)
+}
+
+function divideBase(prime, base) {
+    let counter = 0;
+    while (base >= prime && base % prime === 0) {
+        base /= prime;
+        counter++;
+    }
+    return {base: prime, count: counter};
+}
+
+function getPrimes(base) {
+    var sieve = [], i, j, primes = [];
+    for (i = 2; i <= base; ++i) {
+        if (!sieve[i]) {
+            primes.push(i);
+            for (j = i << 1; j <= base; j += i) {
+                sieve[j] = true;
+            }
+        }
+    }
+    return primes;
 }
